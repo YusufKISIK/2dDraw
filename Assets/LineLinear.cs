0726840faa44cee5a215da9f96a1db61907f29ff
Yusuf Kazım Işık <yusufkazimisik@Baustem-iMac.local>
2018-10-24 18:17:30 +0300

    initial commit

---
+++ Assets/LineLinear.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineLinear : MonoBehaviour
+{
+	private LineRenderer lineR;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		lineR = GetComponent<LineRenderer>();
+		
+		Vector2 mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		
+		lineR.SetPosition(0,mousepos);
+		
+	}
+
+	public void SetLastPoint(Vector2 position)
+	{
+		
+		if (lineR != null)
+		{
+			lineR.SetPosition(1,position);	
+		}
+	}
+
+	public void Destroy()
+	{
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/lineCreating.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lineCreating : MonoBehaviour {
+
+	public int LineFadeTime;
+
+	static public bool MouseButtonUp = false;
+
+	public GameObject linePrefab;
+
+	public static LineLinear activeLine;
+
+	private void Start()
+	{
+		
+	}
+
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			RaycastHit2D hit = Physics2D.Raycast(mousePos, -Vector2.up);
+			if (hit.collider != null)
+			{
+				Debug.Log("HIT");
+				if (hit.collider.tag.Equals("dot")) {
+					Debug.Log("HIT - DOT");
+					GameObject lineGo = Instantiate(linePrefab);
+					activeLine = lineGo.GetComponent<LineLinear>();
+				}
+			}
+			
+			
+			
+			
+		}
+		if (Input.GetMouseButton(0))
+		{
+			if (activeLine != null)
+			{
+				Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+				activeLine.SetLastPoint(mousePos);
+			}
+		}
+		if (Input.GetMouseButtonUp(0))
+		{
+			Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			RaycastHit2D hit = Physics2D.Raycast(mousePos, -Vector2.up);
+			if (hit.collider != null)
+			{
+				if (hit.collider.tag.Equals("dot"))
+				{
+					activeLine = null;
+				}
+			}
+			else
+			{
+				
+				activeLine.Destroy();
+				activeLine = null;
+			}
+		}
+	}
+}
